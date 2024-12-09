c9872994211cdd345b3566e3f7fdb2fc0398a196
KelseyG1012 <kelsey370@gmail.com>
2024-12-09 09:55:29 -0800

    migrations done

--- backend/migrations/20241205200315_database_creation.js
+++ backend/migrations/20241205200315_database_creation.js
@@ -4,59 +4,32 @@
  */
 exports.up = function(knex) {
   return knex.schema
-    .createTable('ability_tbl', function (table) {
+  .createTable('departure_tbl', function (table) {
+    table.increments('id');
+    table.string('name');
+
+  })
+
+  .createTable('drop_zone_tbl', function (table) {
+    table.increments('id');
+    table.string('name');
+
+  })
+
+    .createTable('flight_tbl', function (table) {
       table.increments('id');
-      table.string('ability_name', 25);
-      table.timestamps(true, true);
-    })
-    .createTable('type_tbl', function (table) {
-      table.increments('id');
-      table.string('type_name', 25);
-      table.timestamps(true, true);
-    })
-    .createTable('pkmn_stats_tbl', function (table) {
-      table.increments('id');
-      table.integer('hitpoints');
-      table.integer('attack');
-      table.integer('defence');
-      table.integer('spec_attack');
-      table.integer('spec_defence');
-      table.integer('speed');
-      table.timestamps(true, true);
-    })
-    .createTable('pokemon_data_tbl', function (table) {
-      table.increments('id');
-      table.string('Name', 50);
-      table.integer('Height');
-      table.integer('Weight');
-      table.integer('Level');
-      table.integer('Experience');
-      table.integer('stats_tbl_id');
-      table.foreign('stats_tbl_id').references('id').inTable('pkmn_stats_tbl').onDelete('cascade');
-      table.timestamps(true, true);
-    })
-    .createTable('pkmn_type_ref_tbl', function (table) {
-      table.increments('id');
-      table.integer('pkmn_id');
-      table.foreign('pkmn_id').references('id').inTable('pokemon_data_tbl').onDelete('cascade');
-      table.integer('type_1');
-      table.foreign('type_1').references('id').inTable('type_tbl').onDelete('cascade');
-      table.integer('type_2');
-      table.foreign('type_2').references('id').inTable('type_tbl').onDelete('cascade');
-      table.timestamps(true, true);
-    })
-    .createTable('pkmn_ability_ref_tbl', function (table) {
-      table.increments('id');
-      table.integer('pkmn_id');
-      table.foreign('pkmn_id').references('id').inTable('pokemon_data_tbl').onDelete('cascade');
-      table.integer('slot_1');
-      table.foreign('slot_1').references('id').inTable('ability_tbl').onDelete('cascade');
-      table.integer('slot_2');
-      table.foreign('slot_2').references('id').inTable('ability_tbl').onDelete('cascade');
-      table.integer('slot_3');
-      table.foreign('slot_3').references('id').inTable('ability_tbl').onDelete('cascade');
-      table.timestamps(true, true);
-    })
+      table.string('airframe');
+      table.string('type_tod');
+      table.string('type_load');
+      table.integer('departure_id');
+      table.foreign('departure_id').references('departure_tbl.id');
+      table.string('drop_zone_id');
+      table.foreign('drop_zone_id').references('drop_zone_tbl.id');
+      table.timestamp('date_time');
+      table.integer('number_pax');
+      table.integer('number_passes');
+  })
+
 };
 
 /**
@@ -65,27 +38,17 @@ exports.up = function(knex) {
  */
 exports.down = function(knex) {
   return knex.schema
-    .alterTable('pkmn_ability_ref_tbl', table => {
-      table.dropForeign('slot_3')
-      table.dropForeign('slot_2')
-      table.dropForeign('slot_1')
-      table.dropForeign('pkmn_id')
+    .alterTable('flight_tbl', table => {
+      table.dropForeign('departure_id')
+      table.dropForeign('drop_zone_id')
+
     })
-    .alterTable('pkmn_type_ref_tbl', table => {
-      table.dropForeign('type_2')
-      table.dropForeign('type_1')
-      table.dropForeign('pkmn_id')
-    })
-    .alterTable('pokemon_data_tbl', table => {
-      table.dropForeign('stats_tbl_id')
-    })
+
     .then( function () {
       return knex.schema
-        .dropTableIfExists('type_tbl')
-        .dropTableIfExists('pkmn_stats_tbl')
-        .dropTableIfExists('pkmn_type_ref_tbl')
-        .dropTableIfExists('pokemon_data_tbl')
-        .dropTableIfExists('pkmn_ability_ref_tbl')
-        .dropTableIfExists('ability_tbl')
+      .dropTableIfExists('flight_tbl')
+      .dropTableIfExists('drop_zone_tbl')
+      .dropTableIfExists('departure_tbl')
+
     })
 };
