f35619a9c86c5c73643afcfb95c59ea58dbeb0da
leanXII <jctuttle42@gmail.com>
2024-12-10 11:27:56 -0500

    added yaml to gitigore

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const session = require('express-session')
-const passport = require('passport')
-const authRouter = require('./routes/auth.js')
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
+const authRouter = require("./routes/auth.js");
 const userRouter = require('./routes/user_training')
 
 const app = express();
@@ -12,19 +12,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(session({
-  secret: 'Airborne',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false,
-    httpOnly: true,
-    sameSite: 'lax'
-  }
-}))
+app.use(
+  session({
+    secret: "Airborne",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 36000,
+    },
+  })
+);
 
 app.use(passport.initialize());
-app.use(passport.authenticate('session'));
+app.use(passport.authenticate("session"));
 
 //Routes
 app.use('/api/Individual-Training-Record', userRouter);
@@ -33,9 +36,7 @@ app.use('/oauth2', authRouter);
 
 //General | Root Route
 app.get("/", (req, res) => {
-  res.send("Express API Application is up and running")
-})
-
-
+  res.send("Express API Application is up and running");
+});
 
 module.exports = app;
