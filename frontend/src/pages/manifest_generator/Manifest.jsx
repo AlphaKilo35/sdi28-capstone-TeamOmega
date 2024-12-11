24639b3ba7557a9a3ce770d39f5bf8a57d7f5d27
CatCode23 <andrewcatalano23@gmail.com>
2024-12-11 12:16:27 -0500

    pulling Nephi's changes

--- frontend/src/pages/manifest_generator/Manifest.jsx
+++ frontend/src/pages/manifest_generator/Manifest.jsx
@@ -127,7 +127,7 @@ export default function Manifest({ totalSeats = 20, flightId = 2 }) {
                 <button
                   onClick={() => addToManifest(jumper)}
                   disabled={manifestJumpers.length >= totalSeats}
-                  className="bg-blue-500 text-white"
+                  className="bg-blue-500 text-white cursor-pointer"
                 >
                   add
                 </button>
@@ -169,7 +169,7 @@ export default function Manifest({ totalSeats = 20, flightId = 2 }) {
               </div>
               <button
                 onClick={() => removeFromManifest(jumper)}
-                className="bg-red-500 text-white"
+                className="bg-red-500 text-white cursor-pointer"
               >
                 remove
               </button>
