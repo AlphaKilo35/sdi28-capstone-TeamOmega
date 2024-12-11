06ca9e3f8432e0f0e3d774f554dbc4a7e58047b7
Connor <connorjtaberski@gmail.com>
2024-12-11 14:21:14 -0500

    Updated fetch URL

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import './home.css'
 function Home() {
   let userId = 1
   useEffect(() => {
-    fetch(`localhost:3000/users/${userId}`)
+    fetch(`http://localhost:3000/users/${userId}`)
     .then(res => res.json())
     .then(data => console.log(data))
   })
