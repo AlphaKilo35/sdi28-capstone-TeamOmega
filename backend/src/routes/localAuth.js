abb643544b6618e8f266c558d1313a471f421c76
leanXII <jctuttle42@gmail.com>
2024-12-13 10:26:11 -0500

    previousLogin value updates for either login method

--- backend/src/routes/localAuth.js
+++ backend/src/routes/localAuth.js
@@ -36,14 +36,18 @@ router.post("/login", (req, res, next) => {
     req.logIn(user, (err) => {
       if (err) return res.status(500).json({ message: "Login error" });
       if (req.isAuthenticated() && req.user) {
-        res.status(200).json({ redirectUrl: "/" });
+        knex("users")
+          .update({ previousLogin: true })
+          .where({ id: req.user.id })
+          .then(() => {
+            res.status(200).json({ redirectUrl: "/" });
+          });
       }
     });
   })(req, res, next);
 });
 
 router.post("/signup", (req, res) => {
-
   const { username, password, admin, authCode } = req.body;
   console.log(req.body);
   const salt = bcrypt.genSaltSync(10);
@@ -56,19 +60,18 @@ router.post("/signup", (req, res) => {
           .insert({ username: username, password: hash, role: "User" })
           .returning("*");
         console.log(user);
-        res.status(200).json({success: true, code: 0});
+        res.status(200).json({ success: true, code: 0 });
       } else if (admin && authCode === process.env.ADMIN_AUTH_STRING) {
         const user = await knex("users")
           .insert({ username: username, password: hash, role: "Admin" })
           .returning("*");
         console.log(user);
-        res.status(200).json({success:true, code: 0});
-
+        res.status(200).json({ success: true, code: 0 });
       } else {
-        res.status(404).json({success:false, code: 1 });
+        res.status(404).json({ success: false, code: 1 });
       }
     } catch (err) {
-      res.status(500).json({success:false, code: 2});
+      res.status(500).json({ success: false, code: 2 });
     }
   };
   createUser();
