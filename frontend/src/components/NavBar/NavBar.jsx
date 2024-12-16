c5fc155377fbdad5e823bfba3ae53a6bed30f824
leanXII <jctuttle42@gmail.com>
2024-12-16 17:14:52 -0500

    added dropdown for logout and profile view

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ import "./nav-bar.css";
 
 function NavBarComponent() {
   const [user, setUser] = useState("");
-
+  const [dropdownSelect, setDropdownSelect] = useState(false);
   let navigate = useNavigate();
 
   useEffect(() => {
@@ -31,8 +31,18 @@ function NavBarComponent() {
   });
 
   function handleNavigate(link) {
-    console.log(link)
+    console.log(link);
     navigate(`/${link}`);
+  }
+
+  const handleLogout = () => {
+    fetch('http://localhost:3000/local/logout', {
+      method: 'POST',
+      "Content-Type": "application/json",
+      credentials: "include"
+    })
+    .then((res)=>res.json())
+    .then((data)=> data ? navigate('/login') : console.error('Server Error'))
   }
 
   return (
@@ -46,21 +56,41 @@ function NavBarComponent() {
         <div className="absolute ml-20 flex justify-between w-full">
           <ul className="space-x-4 flex mb-2">
             <li className="ml-4 cursor-pointer">
-              <h3 onClick={() => handleNavigate('home')}>Home</h3>
+              <h3 onClick={() => handleNavigate("home")}>Home</h3>
             </li>
             <li className="ml-4 cursor-pointer">
-              <h3 onClick={() => handleNavigate(`profiles/${user.id}`)}>Profile</h3>
+              <h3 onClick={() => handleNavigate("flights")}>Flights</h3>
             </li>
             <li className="ml-4 cursor-pointer">
-              <h3 onClick={() => handleNavigate('flights')}>Flights</h3>
-            </li>
-            <li className="ml-4 cursor-pointer">
-              <h3 onClick={() => handleNavigate(`Individual-Training-Record/${user.id}`)}>Training Record</h3>
+              <h3
+                onClick={() =>
+                  handleNavigate(`Individual-Training-Record/${user.id}`)
+                }
+              >
+                Training Record
+              </h3>
             </li>
           </ul>
           <div className=" absolute right-28 flex ">
             <CircleUserRound />
-            <p className="ml-2">{user}</p>
+            <div className="relative">
+              <button
+                className="ml-2 z-10 cursor-pointer"
+                onClick={() => setDropdownSelect(!dropdownSelect)}
+              >
+                {user}
+              </button>
+              <div
+                className={`left-3 top-8 transform transition-all duration-300 ease-out absolute rounded-md p-2 text-sm border border-gold-400 bg-black ${
+                  dropdownSelect
+                    ? "opacity-100 translate-y-0"
+                    : "opacity-0 invisable translate-y-[-20px] pointer-events-none"
+                }`}
+              >
+                <div className="p-1 h-full cursor-pointer" onClick={()=>navigate('/profiles')}>Profile</div>
+                <div className="p-1 h-full text-nowrap cursor-pointer" onClick={handleLogout}>Log out</div>
+              </div>
+            </div>
           </div>
         </div>
       </nav>
