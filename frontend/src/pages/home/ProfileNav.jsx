a3a011ee48fb7778610552075e035af3240bad9c
Connor <connorjtaberski@gmail.com>
2024-12-17 10:43:56 -0500

    Fixed profile navigation issues

--- frontend/src/pages/home/ProfileNav.jsx
+++ frontend/src/pages/home/ProfileNav.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, use } from "react";
 import "./home.css";
 import { UserContext } from "./Home.jsx";
 import { useNavigate } from "react-router-dom";
@@ -8,21 +8,21 @@ function ProfileNav() {
 
   const navigate = useNavigate();
   function handleProfileNavigate() {
-    navigate(`/profiles`);
+    navigate(`/profile`);
   }
 
   return (
-    <div class="group h-96 w-80 [perspective:1000px]">
-        <div class="relative h-full w-full rounded-xl z-10 shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
-          <div class="absolute">
-            <img class="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40" src="/Soldier.jpg" alt="Silhoutte of a Soldier" />
+    <div className="group h-96 w-80 [perspective:1000px]">
+        <div className="relative h-full w-full rounded-xl z-10 shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+          <div className="absolute">
+            <img className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40" src="/Soldier.jpg" alt="Silhoutte of a Soldier" />
           </div>
-          <div class="absolute h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
-            <div class="flex min-h-full flex-col items-center justify-center">
-              <h1 class="text-3xl font-bold">User Profile</h1>
-              <p class="text-lg">Update</p>
-              <p class="text-base">Admin Rights</p>
-              <button class="mt-2 rounded-md bg-neutral-800 py-1 px-2 text-sm hover:bg-neutral-900" onClick={handleProfileNavigate}>View</button>
+          <div className="absolute h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+            <div className="flex min-h-full flex-col items-center justify-center">
+              <h1 className="text-3xl font-bold">User Profile</h1>
+              <p className="text-lg">Update</p>
+              <p className="text-base">Admin Rights</p>
+              <button className="mt-2 rounded-md bg-neutral-800 py-1 px-2 text-sm hover:bg-neutral-900" onClick={handleProfileNavigate}>View</button>
             </div>
           </div>
         </div>
--- frontend/src/pages/home/TrainingRecordNav.jsx
+++ frontend/src/pages/home/TrainingRecordNav.jsx
@@ -12,17 +12,17 @@ function TrainingRecordNav() {
   }
   if (user.role === "admin" || user.role === "leader") {
     return (
-      <div class="group h-96 w-80 [perspective:1000px]">
-        <div class="relative h-full w-full rounded-xl z-10 shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
-          <div class="absolute">
-            <img class="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40" src="/Training.jpg" alt="Silhoutte of a Paratrooper" />
+      <div className="group h-96 w-80 [perspective:1000px]">
+        <div className="relative h-full w-full rounded-xl z-10 shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+          <div className="absolute">
+            <img className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40" src="/Training.jpg" alt="Silhoutte of a Paratrooper" />
           </div>
-          <div class="absolute h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
-            <div class="flex min-h-full flex-col items-center justify-center">
-              <h1 class="text-3xl font-bold">Training Record</h1>
-              <p class="text-lg">Jump Log</p>
-              <p class="text-base">Current Jump Status</p>
-              <button class="mt-2 rounded-md bg-neutral-800 py-1 px-2 text-sm hover:bg-neutral-900" onClick={handleTrainingRecordNavigate}>View</button>
+          <div className="absolute h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+            <div className="flex min-h-full flex-col items-center justify-center">
+              <h1 className="text-3xl font-bold">Training Record</h1>
+              <p className="text-lg">Jump Log</p>
+              <p className="text-base">Current Jump Status</p>
+              <button className="mt-2 rounded-md bg-neutral-800 py-1 px-2 text-sm hover:bg-neutral-900" onClick={handleTrainingRecordNavigate}>View</button>
             </div>
           </div>
         </div>
