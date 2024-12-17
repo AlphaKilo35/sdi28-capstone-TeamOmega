4b4e998a24f0c9dad98545a264f36e2923141b80
alphakilo35 <legolas9292@yahoo.com>
2024-12-17 15:34:16 -0500

    Fixed logic on Currency Time Statement

--- frontend/src/pages/IndividualTraining/PayLoss.jsx
+++ frontend/src/pages/IndividualTraining/PayLoss.jsx
@@ -3,13 +3,11 @@ import { Link } from 'react-router-dom';
 import {trainingContext} from './IndividualTrainingDashboard.jsx'
 
 let checkCurrency = (date) => {
-
   let currentMonth = new Date().getMonth();
   let currentYear = new Date().getFullYear();
   let currentQuarter = Math.floor(currentMonth / 3) + 1;
   let checkedQuarter = Math.floor(new Date(date).getMonth() / 3) + 1;
   let checkedYear = new Date(date).getFullYear();
-  console.log (currentMonth, currentYear, currentQuarter, checkedQuarter, checkedYear)
   if ( (checkedQuarter === currentQuarter || checkedQuarter === currentQuarter -1) &&
         ( checkedYear === currentYear || checkedYear === currentYear - 1) ) {
     return true;
@@ -17,31 +15,47 @@ let checkCurrency = (date) => {
   return false;
 }
 
+let checkJumps = (date) => {
+  let currentMonth = new Date().getMonth();
+  let currentYear = new Date().getFullYear();
+  let currentQuarter = Math.floor(currentMonth / 3) + 1;
+  let checkedQuarter = Math.floor(new Date(date).getMonth() / 3) + 1;
+  let checkedYear = new Date(date).getFullYear();
+  console.log((currentYear - checkedYear) * 4 + (currentQuarter - checkedQuarter))
+  return ( (currentYear - checkedYear) * 4 + (currentQuarter - checkedQuarter))
+}
+
+let daysLeft = () => {
+  let now = new Date();
+  let currentQuarter = Math.floor(now.getMonth() / 3) + 1;
+  let quarterEndDate;
+  switch (currentQuarter) {
+    case 1: quarterEndDate = new Date(now.getFullYear(), 2, 31);
+      break;
+    case 2: quarterEndDate = new Date(now.getFullYear(), 5, 30);
+      break;
+    case 3: quarterEndDate = new Date(now.getFullYear(), 8, 30);
+      break;
+    case 4: quarterEndDate = new Date(now.getFullYear(),11,31);
+      break;
+  }
+  return ( `${quarterEndDate.toISOString().split('T')[0]} (${Math.ceil( (quarterEndDate - now) / (1000*3600*24))})`);
+}
+
 const Pay_Tracker = () => {
   const [isCurrent, setIsCurrent] = useState(true);
-  const [jumpsNeeded, setJumpsNeeded] = useState(3); // Replace with real data
+  const [jumpsNeeded, setJumpsNeeded] = useState(1); // Replace with real data
   const [daysUntilPayLoss, setDaysUntilPayLoss] = useState(5); // Replace with real data
 
   let jumpData = useContext(trainingContext);
 
-  // if (!jumpData.loading && !(jumpData.dataObject === null || jumpData.dataObject === undefined) ) {
-  //   //Object.keys(jumpData.dataObject).length === 0 ) {
-  //   //Array.isArray(jumpData.dataObject)) {
-  //     var lastJump = jumpData.dataObject.filter(item => item.status === 'complete').reduce((latest, current) => {
-  //       return new Date(current.date_time) > new Date(latest.date_time) ? current : latest;
-  //     });
-  // } else {
-  //   var lastJump = {date_time: ''};
-  //   lastJump.date_time = new Date('1990-01-01').toISOString()
-  // }
-
   useEffect( () => {
     if (!jumpData.loading && jumpData.dataObject.filter(item => item.status === 'complete').length > 0) {
-      console.log(jumpData.dataObject)
       var lastJump = jumpData.dataObject.filter(item => item.status === 'complete').reduce((latest, current) => {
             return new Date(current.date_time) > new Date(latest.date_time) ? current : latest;
       });
-      setIsCurrent(checkCurrency(lastJump.date_time.split('T')[0]))
+      setIsCurrent(checkCurrency(lastJump.date_time.split('T')[0]));
+      setJumpsNeeded(checkJumps(lastJump.date_time.split('T')[0]));
     } else {
       setIsCurrent(false);
     }
@@ -65,25 +79,9 @@ const Pay_Tracker = () => {
       >
         {isCurrent ? "Current" : "Not Current"}
       </div>
-
-      {/* Jumps Needed to Become Current */}
-      {!isCurrent && (<>
-        <div>
-          <p>You need {jumpsNeeded} more jump(s) to become current.</p>
-        </div>
-
-        <div className="mb-4">
-          <p>
-          { daysUntilPayLoss > 0
-            ? `You have ${daysUntilPayLoss} day(s) to find a jump before pay loss.`
-            : "Pay loss incurred!"}
-          </p>
-        </div>
-        </>
-      )}
-
-
-
+      <div>
+      <p>You need <b>{jumpsNeeded}</b> more jump(s) to maintain / become current by: <b>{daysLeft()}</b> days.</p>
+      </div>
       {/* Link to Airborne Calendar */}
       <div>
           <Link className=" font-semibold text-blue-600 underline" to="/Flights">Flights</Link>
